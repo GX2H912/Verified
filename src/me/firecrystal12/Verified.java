e5e9280b9660d3c1412daa346f357b80e28670ed
Mandar Ghade <GX2H912@gmail.com>
2016-04-08 20:20:07 -0400

    Added files via upload
    Added my Verify plugin (very similar to TotalFreedom's)

---
+++ src/me/firecrystal12/Verified.java
@@ -0,0 +1,35 @@
+package me.firecrystal12;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Verified extends JavaPlugin{
+	
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("FireCrystal12's Verifyer has been enabled, and is similar to TotalFreedom's");
+	}
+	
+	@Override
+	public void onDisable() {
+
+		}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(cmd.getName().equalsIgnoreCase("verify") && sender instanceof Player) {
+			
+			Player player = (Player) sender;
+			
+			player.sendMessage(ChatColor.DARK_AQUA + "YOU HAVE BEEN FOUND TO BE AN IMPOSTER!" + ChatColor.DARK_RED + player.getName() + "," + ChatColor.RED + "Verify at " + ChatColor.RED + "chickenfreedom.boards.net");
+			
+			return true;
+		}
+		return false;
+	}
+
+}
